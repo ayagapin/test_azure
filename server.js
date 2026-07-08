aea5837f465f87fc5090c139d6836595732f6b4a
Achille_YAGAPIN <ayagapin@oxyl.fr>
2026-07-08 11:48:12 +0200

    Ajout du mécanisme de variable d'environnement

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 8080;
 // Sert tous les fichiers statiques (HTML, CSS, images) du dossier actuel
 app.use(express.static(path.join(__dirname)));
 
+// Route API pour envoyer notre variable d'environnement au navigateur
+app.get('/api/message', (req, res) => {
+  res.json({ message: process.env.MY_SECRET_MESSAGE || "Message par défaut (développement local)" });
+});
+
 // Pour toute autre requête, on renvoie le fichier index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
